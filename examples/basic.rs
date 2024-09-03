3c8f6d6dbd7726a63ae34841b027ba6de7d561b2
Roee Shoshani <roysho10@gmail.com>
2024-09-03 16:45:27 -0400

    parse legacy prefixes

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,6 @@
 use pis::Context;
 fn main() {
     let ctx = Context::new();
-    let res = ctx.translate(&[0x50]);
+    let res = ctx.translate(&[0xf0, 0x66, 0x50]);
     println!("{}", res);
 }
--- src/x86.rs
+++ src/x86.rs
@@ -1,6 +1,9 @@
+use std::path::StripPrefixError;
+
 use crate::{Insn, Opcode, Operand, OperandAddr, OperandSize, OperandSpace, Translation};
 use bitpiece::{bitpiece, BitPiece, BitStorage};
 use paste::paste;
+use strum::{EnumIter, IntoEnumIterator};
 
 macro_rules! define_reg_operand {
     {$name: ident, $offset: expr, $size: ident} => {
@@ -78,6 +81,76 @@ impl Reg {
     }
 }
 
+#[derive(EnumIter, Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum LegacyPrefixGroup {
+    Group1,
+    Group2,
+    Group3,
+    Group4,
+}
+impl LegacyPrefixGroup {
+    pub const GROUPS_AMOUNT: usize = 4;
+    pub const fn index(&self) -> usize {
+        *self as usize
+    }
+}
+
+#[derive(EnumIter, Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum LegacyPrefix {
+    Lock = 0xf0,
+    Repnz = 0xf2,
+    RepOrRepz = 0xf3,
+
+    CsSegmentOverrideOrBranchNotTaken = 0x2e,
+    SsSegmentOverride = 0x36,
+    DsSegmentOverrideOrBranchTaken = 0x3e,
+    EsSegmentOverride = 0x26,
+    FsSegmentOverride = 0x64,
+    GsSegmentOverride = 0x65,
+
+    OperandSizeOverride = 0x66,
+
+    AddressSizeOverride = 0x67,
+}
+impl LegacyPrefix {
+    pub const fn group(&self) -> LegacyPrefixGroup {
+        match self {
+            Self::Lock | Self::Repnz | Self::RepOrRepz => LegacyPrefixGroup::Group1,
+            Self::CsSegmentOverrideOrBranchNotTaken
+            | Self::SsSegmentOverride
+            | Self::DsSegmentOverrideOrBranchTaken
+            | Self::EsSegmentOverride
+            | Self::FsSegmentOverride
+            | Self::GsSegmentOverride => LegacyPrefixGroup::Group2,
+            Self::OperandSizeOverride => LegacyPrefixGroup::Group3,
+            Self::AddressSizeOverride => LegacyPrefixGroup::Group4,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct InsnLegacyPrefixes {
+    pub by_group: [Option<LegacyPrefix>; LegacyPrefixGroup::GROUPS_AMOUNT],
+}
+impl InsnLegacyPrefixes {
+    pub fn add(&mut self, prefix: LegacyPrefix) {
+        let group = prefix.group();
+        let prefix_entry = &mut self.by_group[group.index()];
+        assert!(
+            prefix_entry.is_none(),
+            "multiple legacy prefixes of the same group {:?} - {:?} and {:?}",
+            group,
+            prefix_entry.unwrap(),
+            prefix
+        );
+
+        *prefix_entry = Some(prefix);
+    }
+    pub fn contains(&self, prefix: LegacyPrefix) -> bool {
+        self.by_group[prefix.group().index()] == Some(prefix)
+    }
+}
+
 fn translate_push_reg(reg: Reg) -> Translation {
     let mut translation = Translation::new();
     translation.insns.push(Insn::new(
@@ -91,9 +164,30 @@ fn translate_push_reg(reg: Reg) -> Translation {
     translation
 }
 
-pub fn translate(code: &[u8]) -> Translation {
+fn extract_legacy_prefixes(code: &mut &[u8]) -> InsnLegacyPrefixes {
+    let mut prefixes = InsnLegacyPrefixes {
+        by_group: [None; LegacyPrefixGroup::GROUPS_AMOUNT],
+    };
+    while !code.is_empty() {
+        let Some(matching_prefix) = LegacyPrefix::iter().find(|prefix| code[0] == *prefix as u8)
+        else {
+            // non-prefix byte, so we are done parsing the prefixes
+            break;
+        };
+
+        prefixes.add(matching_prefix);
+
+        // advance by 1 byte
+        *code = &code[1..];
+    }
+
+    prefixes
+}
+
+pub fn translate(mut code: &[u8]) -> Translation {
+    let legacy_prefixes = extract_legacy_prefixes(&mut code);
+    println!("prefixes: {:?}", legacy_prefixes);
     if code.len() == 1 && code[0] >= 0x50 && code[0] <= 0x50 + Reg::MAX_VALUE as u8 {
-        // push reg instruction
         return translate_push_reg(Reg::from_bits(code[0] - 0x50));
     }
     todo!()
